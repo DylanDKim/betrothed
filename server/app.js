354e5a5fed257613cb1abf44408a86dcb54fc08e
Adam Klingbaum <adam.klingbaum@gmail.com>
2021-12-08 18:26:20 -0500

    update server routers

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const {
   couplesRouter,
   guestsRouter,
   eventsRouter,
-  rspvsRouter,
+  rsvpsRouter,
 } = require('./routers');
 
 // Middleware
--- server/routers/rsvps.js
+++ server/routers/rsvps.js
@@ -1,4 +1,5 @@
-const router = require('express');
+const express = require('express');
+const router = express.Router();
 
 router.get('/', (req, res) => {
   res.status(200).send('hello from /api/rsvps');
