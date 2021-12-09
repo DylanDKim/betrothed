34b2bdc3711b80242b9433711f67e321d75aab29
Adam Klingbaum <adam.klingbaum@gmail.com>
2021-12-09 14:52:21 -0500

    move out server code - separate repo

--- server/app.js
+++ server/app.js
@@ -12,10 +12,4 @@ app.use(express.static('dist'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Routers
-app.use('/api/couples', couplesRouter);
-app.use('/api/guests', guestsRouter);
-app.use('/api/events', eventsRouter);
-app.use('/api/rsvps', rsvpsRouter);
-
 module.exports = app;
--- server/models/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  couple: require('./couple'),
-  guest: require('./guest'),
-  event: require('./event'),
-  rsvp: require('./rsvp'),
-};
