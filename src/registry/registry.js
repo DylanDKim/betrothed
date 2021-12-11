8cb754bc485a74786a20ecabef7d2cf8ecaa87b9
Khai Tran <kqtran917@yahoo.com>
2021-12-11 16:02:19 -0600

    Implement registry start mock up inline styles

--- src/registry/registry.js
+++ src/registry/registry.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Col, Row } from 'react-bootstrap';
+import { Button, Container, Col, Row } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Registry() {
@@ -7,10 +7,27 @@ export default function Registry() {
     <>
       {' '}
       <Container>
-        <Row>
-          <Col sm={8} md={8}>
-            <Row className="mt-5">
-              <h1 style={{ fontFamily: 'Faustina serif' }}>Registry</h1>
+        <Row style={{ height: '100vh' }}>
+          <Col
+            className="border d-flex align-items-center justify-content-center"
+            sm={8}
+            md={8}
+          >
+            <Row>
+              <h1>Progress bar holder</h1>
+              <h1 style={{ fontFamily: 'Alex Brush' }}>Registry</h1>
+              <h2 style={{ fontFamily: 'Faustina serif' }}>
+                Welcome to your registry!
+              </h2>
+              <p style={{ fontFamily: 'Merriweathe' }}>
+                Browse registry essentials, unique gifts, and more!
+                <br />
+                Mark your top picks so guests can easily find your favorites
+                <br />
+                Add gifts simply by adding the url
+              </p>
+
+              <Button className="w-25">Next</Button>
             </Row>
           </Col>
           <Col sm={4} md={4}>
