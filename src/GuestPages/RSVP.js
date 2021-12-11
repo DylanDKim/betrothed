b816e49e68c398a6f010803e3a76deba0b53d700
kyle_sablan <kyle.sablan@gmail.com>
2021-12-11 15:34:39 -0800

    Added forms for inputting RSVP response

--- src/GuestPages/RSVP.js
+++ src/GuestPages/RSVP.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Container, Button } from 'react-bootstrap';
+import {
+  Container,
+  Button,
+  Form,
+  DropdownButton,
+  Dropdown,
+} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const RsvpForm = () => (
@@ -13,9 +19,44 @@ const RsvpForm = () => (
     <Container>Date</Container>
     <Container>Time</Container>
     <Container>Location</Container>
-    <Link to="/guest-invite">
-      <Button>Back to invite</Button>
-    </Link>
+    <Container>
+      <Form>
+        <Form.Group controlId="guestEmail">
+          <Form.Label>Check RSVP Status</Form.Label>
+          <Form.Control type="email" placeholder="enter email" />
+        </Form.Group>
+      </Form>
+      <Link to="/guest-invite">
+        <Button>Back to invite</Button>
+      </Link>
+    </Container>
+    <Container>
+      <Form>
+        <Form.Group>
+          <Form.Label>Attending</Form.Label>
+          <Form.Select
+            onChange={(e) => {
+              console.log(e.target.value);
+            }}
+          >
+            <option selected disabled>
+              select from options
+            </option>
+            <option value="yes">Yes!</option>
+            <option value="no">No</option>
+          </Form.Select>
+          <Form.Label>Message to the couple (optional)</Form.Label>
+          <Form.Control as="textarea" placeholder="enter message" />
+        </Form.Group>
+      </Form>
+      <Button
+        onClick={() => {
+          console.log('rsvping');
+        }}
+      >
+        RSVP
+      </Button>
+    </Container>
   </>
 );
 
