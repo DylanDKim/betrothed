873dc52716cc6dd64054a0e6ce4c7c22eb6d12e4
Khai Tran <kqtran917@yahoo.com>
2021-12-11 17:35:31 -0600

    remove comments in metadataParser

--- src/registry/utils/metadataParse.js
+++ src/registry/utils/metadataParse.js
@@ -1,13 +1,5 @@
 const { parser } = require('html-metadata-parser');
 
-// HTML Metadata Parser test
-
-// parser(
-//   'https://www.bestbuy.com/site/apple-12-9-inch-ipad-pro-latest-model-with-wi-fi-128gb-space-gray/4263701.p?skuId=4263701'
-// ).then((result) => {
-//   console.log(JSON.stringify(result, null, 3));
-// });
-
 const metadataParser = (url, callback) => {
   parser(url).then((result) => {
     console.log(JSON.stringify(result, null, 3));
