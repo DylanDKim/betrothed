7b0ac519519dde3ee731da0787be27d5ffa97703
Khai Tran <kqtran917@yahoo.com>
2021-12-11 18:39:38 -0600

    implement first styled component button

---
+++ src/Common/styled/BlueButton.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import { Button } from 'react-bootstrap';
+
+const BlueButton = styled(Button)`
+  font-family: 'Faustina';
+  background-color: white;
+  color: #90b0c5;
+  border: 3px solid #90b0c5;
+`;
+
+export default BlueButton;
