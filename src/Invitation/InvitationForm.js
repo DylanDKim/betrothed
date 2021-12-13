f59dd51fd55fcf47e252d1094a96a5750a272d13
junma <junma0704@gmail.com>
2021-12-13 15:05:15 -0800

    Refactor to preview main photo

--- src/Invitation/InvitationForm.js
+++ src/Invitation/InvitationForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import {
   Container,
@@ -14,9 +14,25 @@ import {
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function InvitationForm() {
-  const [mainPhoto, setMainPhoto] = useState('');
+  const [mainPhoto, setMainPhoto] = useState(
+    'http://tineye.com/images/widgets/mona.jpg'
+  );
 
-  const updateMainPhoto = () => {};
+  const hiddenFileInput = useRef(null);
+
+  const handleClick = () => {
+    hiddenFileInput.current.click();
+  };
+
+  const previewMainPhoto = (e) => {
+    const reader = new FileReader();
+    reader.onload = () => {
+      if (reader.readyState === 2) {
+        setMainPhoto(reader.result);
+      }
+    };
+    reader.readAsDataURL(e.target.files[0]);
+  };
 
   return (
     <Container style={{ width: '65%' }}>
@@ -29,23 +45,16 @@ export default function InvitationForm() {
           <Button variant="danger border">Preview Site</Button>
         </Link>
       </Stack>
+      <img src={mainPhoto} alt="testing" width="100%" height="200" />
+      <input
+        type="file"
+        ref={hiddenFileInput}
+        accept="image/*"
+        style={{ display: 'none' }}
+        onChange={previewMainPhoto}
+      />
+      <Button onClick={handleClick}>Choose new photo</Button>
       <Form className="mt-3">
-        <Card>
-          <Card.Body>
-            <Card.Title>Main Photo</Card.Title>
-            <Card.Img
-              style={{ height: '11rem' }}
-              variant="top"
-              src={mainPhoto}
-              alt="Main Photo"
-            />
-            <Button variant="primary">Choose new photo</Button>
-          </Card.Body>
-        </Card>
-        <Form.Group className="mb-3" controlId="formMainPhot">
-          <Form.Label>Choose new photo</Form.Label>
-          <Form.Control type="file" />
-        </Form.Group>
         <Form.Group className="mb-3" controlId="formMessage">
           <Form.Label>Message</Form.Label>
           <Form.Control as="textarea" />
@@ -76,4 +85,18 @@ export default function InvitationForm() {
       </Form>
     </Container>
   );
+}
+
+{
+  /* <Card>
+<Card.Body>
+  <Card.Title>Main Photo</Card.Title>
+  <Card.Img
+    style={{ height: '11rem' }}
+    variant="top"
+    src="http://tineye.com/images/widgets/mona.jpg"
+    alt="Main Photo"
+  />
+</Card.Body>
+</Card> */
 }
