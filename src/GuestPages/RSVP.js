e57c2d696b24d87115d11779002ab467c847f07e
kyle_sablan <kyle.sablan@gmail.com>
2021-12-13 16:21:12 -0800

    Refactor of existing components to better utilize react-bootstrap

--- src/GuestPages/RSVP.js
+++ src/GuestPages/RSVP.js
@@ -1,29 +1,105 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Container, Button } from 'react-bootstrap';
+import { Container, Row, Col, Image } from 'react-bootstrap';
+import styled from 'styled-components';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Status from './Status';
 import NewRsvp from './NewRSVP';
 
 const RsvpForm = () => (
   <Container fluid>
-    <Container fluid>
-      <div>Side Image??</div>
-      <div>Couple1 Name</div>
-      <div>&</div>
-      <div>Couple2 Name</div>
-      <div>invite you to their special day</div>
-      <div>Date</div>
-      <div>Time</div>
-      <div>Location</div>
-      <div>RSVP by Deadline</div>
-      <Link to="/guest-invite">
-        <Button>Back to invite</Button>
-      </Link>
-    </Container>
-    <Status />
-    <NewRsvp />
+    <Row style={{ height: '100vh' }}>
+      <Col md={7} sm={12}>
+        <TempHeader
+          className="d-flex justify-content-center mt-5"
+          style={{
+            fontSize: '36px',
+          }}
+        >
+          Hanna Rosales & Ali Levy
+        </TempHeader>
+        <TempHeader
+          className="d-flex justify-content-center"
+          style={{
+            fontSize: '28px',
+          }}
+        >
+          invite you to celebrate their wedding
+        </TempHeader>
+        <TempSubheader className="d-flex justify-content-center mt-3">
+          Saturday, January 8th, 2022
+        </TempSubheader>
+        <TempSubheader className="d-flex justify-content-center">
+          4:30 PM
+        </TempSubheader>
+        <TempSubheader className="d-flex justify-content-center mt-3">
+          Beaulieu Garden
+        </TempSubheader>
+        <TempSubheader className="d-flex justify-content-center">
+          3142 Azalea Lane
+        </TempSubheader>
+        <TempSubheader className="d-flex justify-content-center">
+          Lane Rutherford, CA, 92314
+        </TempSubheader>
+        <TempSubheader
+          className="d-flex justify-content-center mt-3"
+          style={{ color: 'red' }}
+        >
+          Deadline to RSVP: 12/14/2021
+        </TempSubheader>
+
+        <Status />
+        <footer>
+          <h2
+            className="d-flex justify-content-center"
+            style={{
+              fontFamily: 'Alex Brush',
+              fontSize: '28px',
+              marginTop: '10vh',
+              marginBottom: '0',
+            }}
+          >
+            Betrothed
+          </h2>
+          <p
+            className="d-flex justify-content-center"
+            style={{
+              fontFamily: 'Faustina serif',
+              fontSize: '16px',
+              marginBottom: '0',
+            }}
+          >
+            Welcome to your day, your way
+          </p>
+        </footer>
+      </Col>
+      <Col
+        md={5}
+        sm={12}
+        style={{
+          height: '100vh',
+          overflow: 'hidden',
+          paddingLeft: '0',
+          paddingRight: '0',
+        }}
+      >
+        <Image
+          style={{ maxWidth: '100%', height: 'auto' }}
+          src="https://images.unsplash.com/photo-1457089328109-e5d9bd499191?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1875&q=80"
+          alt="flowers"
+        />
+      </Col>
+    </Row>
   </Container>
 );
+
+const TempHeader = styled.div`
+  font-family: Faustina serif;
+  font-style: italic;
+`;
+
+const TempSubheader = styled(TempHeader)`
+  font-size: 18px;
+`;
 
 export default RsvpForm;
--- src/GuestPages/Status.js
+++ src/GuestPages/Status.js
@@ -1,23 +1,25 @@
 import React, { useState } from 'react';
-import { Container, InputGroup, FormControl } from 'react-bootstrap';
+import { Container, Row, InputGroup, FormControl } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Status = () => {
   const [email, updateEmail] = useState('');
 
   return (
-    <Container fluid>
-      <div>Check RSVP Status</div>
-      <InputGroup>
-        <InputGroup.Text onClick={() => console.log(email)}>
-          Search
-        </InputGroup.Text>
-        <FormControl
-          type="email"
-          placeholder="enter email"
-          onChange={(e) => updateEmail(e.target.value)}
-        />
-      </InputGroup>
+    <Container className="d-flex justify-content-center w-50 mt-3">
+      <Row style={{ fontFamily: 'Merriweather' }}>
+        <div>Check RSVP Status</div>
+        <InputGroup className="d-flex justify-content-center">
+          <InputGroup.Text onClick={() => console.log(email)}>
+            <i className="fa fa-search" />
+          </InputGroup.Text>
+          <FormControl
+            type="email"
+            placeholder="enter email"
+            onChange={(e) => updateEmail(e.target.value)}
+          />
+        </InputGroup>
+      </Row>
     </Container>
   );
 };
