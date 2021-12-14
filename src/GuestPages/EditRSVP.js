9bc07aacfbed75c87ac46e7e64684c7d400ed57d
kyle_sablan <kyle.sablan@gmail.com>
2021-12-14 11:18:37 -0800

    Added flow for allowing guests to see 'existing rsvp' and click and edit button enabling changes to the response

--- src/GuestPages/EditRSVP.js
+++ src/GuestPages/EditRSVP.js
@@ -4,9 +4,14 @@ import { Container, Form, Alert, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const EditRsvp = ({ updateStep }) => {
+  const previouseResponse = {
+    status: 'attending',
+    rsvpNote: 'Looking forward to your special day!!!!',
+  };
+
   const [isEditing, enableEdits] = useState(false);
-  const [isAttending, updateAttendance] = useState(null);
-  const [message, updateMessage] = useState('');
+  const [isAttending, updateAttendance] = useState(previouseResponse.status);
+  const [message, updateMessage] = useState(previouseResponse.rsvpNote);
 
   const checkReply = (e) => {
     if (e.target.value === 'yes') {
@@ -16,15 +21,52 @@ const EditRsvp = ({ updateStep }) => {
     }
   };
 
-  const editRSVP = () => {
+  const submitEdits = () => {
     console.log(isAttending);
     console.log(message);
     console.log('submitting!');
     updateStep('status');
   };
 
+  const editRSVP = () => {
+    enableEdits(true);
+  };
+
+  const cancelEdits = () => {
+    enableEdits(false);
+  };
+
+  if (!isEditing) {
+    return (
+      <Container className="w-100" style={{ fontFamily: 'Merriweather' }}>
+        <Alert className="d-flex justify-content-center" variant="warning">
+          You have already RSVP'd
+        </Alert>
+        <Form className="d-flex justify-content-center align-items-end">
+          <Form.Group className="w-50">
+            <Form.Label className="text-muted mb-0">Attending</Form.Label>
+            <Form.Control as="text" disabled={!isEditing}>
+              {previouseResponse.status === 'attending' ? 'Yes!' : 'No'}
+            </Form.Control>
+            <Form.Label className="text-muted mb-0 mt-4">
+              Message to the couple (optional)
+            </Form.Label>
+            <Form.Control as="textarea" disabled={!isEditing}>
+              {previouseResponse.rsvpNote}
+            </Form.Control>
+          </Form.Group>
+        </Form>
+        <Button
+          className="d-flex justify-content-center text-center w-25"
+          onClick={editRSVP}
+        >
+          Edit
+        </Button>
+      </Container>
+    );
+  }
   return (
-    <Container className="w-100">
+    <Container className="w-100" style={{ fontFamily: 'Merriweather' }}>
       <Alert className="d-flex justify-content-center" variant="warning">
         You have already RSVP'd
       </Alert>
@@ -38,9 +80,6 @@ const EditRsvp = ({ updateStep }) => {
             <option value="yes">Yes!</option>
             <option value="no">No</option>
           </Form.Select>
-          <Form.Control as="text" disabled={!isEditing}>
-            Yes!
-          </Form.Control>
           <Form.Label className="text-muted mb-0 mt-4">
             Message to the couple (optional)
           </Form.Label>
@@ -54,9 +93,15 @@ const EditRsvp = ({ updateStep }) => {
       </Form>
       <Button
         className="d-flex justify-content-center text-center w-25"
-        onClick={editRSVP}
+        onClick={submitEdits}
       >
-        RSVP
+        Edit
+      </Button>
+      <Button
+        className="d-flex justify-content-center text-center w-25"
+        onClick={cancelEdits}
+      >
+        Cancel
       </Button>
     </Container>
   );
