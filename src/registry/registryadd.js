fc4df4b9b913a8c182de4e925d6568aa17571e94
Khai Tran <kqtran917@yahoo.com>
2021-12-14 19:57:30 -0600

    Modal design v1 complete, beginning functionality

--- src/registry/registryadd.js
+++ src/registry/registryadd.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Carousel, Container, Row, Col, Modal, Button } from 'react-bootstrap';
+import { Carousel, Container, Row, Col, Modal } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import PlumFilledButton from '../Common/styled/buttonstyles/PlumFilledButton';
 
@@ -11,7 +11,12 @@ export default function ResgistryAdd() {
   const [modalOn, setModalOn] = useState(false);
 
   const handleClose = () => setModalOn(false);
-  const handleShow = () => setModalOn(true);
+  // const handleShow = () => setModalOn(true);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e);
+  };
 
   return (
     <Container>
@@ -29,7 +34,8 @@ export default function ResgistryAdd() {
                 fontFamily: 'Faustina serif',
               }}
             >
-              0 Gifts Requested
+              {registry.length} {registry.length === 1 ? 'Gift' : 'Gifts'}{' '}
+              Requested
             </h2>
             0 Purchased
           </div>
@@ -60,11 +66,9 @@ export default function ResgistryAdd() {
           Your Top Gifts
         </h2>
         {registry.length === 0 ? (
-          <h3>
-            You have {registry.length} gifts in your registry! Try adding some.
-          </h3>
+          <h3>You have NOOOO gifts in your registry! Try adding some.</h3>
         ) : (
-          <h3>You have 5 gifts in your registry! Try adding some.</h3>
+          <h3>You have {registry.length} gifts in your registry!</h3>
         )}
 
         <button
@@ -93,22 +97,70 @@ export default function ResgistryAdd() {
         >
           Claimed
         </h2>
-        <Button variant="primary" onClick={handleShow}>
-          Launch demo modal
-        </Button>
-        <Modal show={modalOn} onHide={handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>Modal heading</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-              Close
-            </Button>
-            <Button variant="primary" onClick={handleClose}>
-              Save Changes
-            </Button>
-          </Modal.Footer>
+        <Modal show={modalOn} closeButton={false} onHide={handleClose}>
+          <Modal.Body style={{ backgroundColor: '#8B5B6E' }}>
+            <Row>
+              <h3
+                style={{
+                  color: 'white',
+                  fontSize: '36px',
+                  fontFamily: 'Faustina serif',
+                }}
+              >
+                Add more gifts
+              </h3>
+            </Row>
+            <Row>
+              <form id="registry" onSubmit={(e) => handleSubmit(e)}>
+                <Row>
+                  <Col>
+                    <label htmlFor="category">
+                      <span
+                        style={{
+                          color: 'white',
+                          fontSize: '24px',
+                          fontFamily: 'Faustina serif',
+                        }}
+                      >
+                        category
+                      </span>
+                      <br />
+                      <input type="text" name="category" />
+                    </label>
+                  </Col>
+                  <Col>
+                    <label htmlFor="url">
+                      <span
+                        style={{
+                          color: 'white',
+                          fontSize: '24px',
+                          fontFamily: 'Faustina serif',
+                        }}
+                      >
+                        url
+                      </span>
+                      <br />
+                      <input type="text" name="url" />
+                    </label>
+                  </Col>
+                </Row>
+              </form>
+            </Row>
+            <Row>
+              <Col>
+                <PlumFilledButton
+                  type="submit"
+                  form="registry"
+                  style={{ float: 'right', border: '1px solid white' }}
+                  className="float-right mt-3"
+                  variant="primary"
+                  onClick={handleClose}
+                >
+                  Save Changes
+                </PlumFilledButton>
+              </Col>
+            </Row>
+          </Modal.Body>
         </Modal>
       </Row>
     </Container>
