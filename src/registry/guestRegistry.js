3189998719551edc301b97039907c25ea3cf5b49
Khai Tran <kqtran917@yahoo.com>
2021-12-15 20:19:01 -0600

    Implemented card disappearing when claimed

--- src/registry/guestRegistry.js
+++ src/registry/guestRegistry.js
@@ -1,101 +1,134 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Row, Card, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import PlumButton from '../Common/styled/buttonstyles/PlumButton';
 
 export default function GuestRegistry() {
+  // const [laptop, setLaptop] = useState(false);
+  // const [blender, setBlender] = useState(false);
+  // const [drip, setDrip] = useState(false);
+
+  const [allValues, setAllValues] = useState({
+    laptop: true,
+    blender: true,
+    drip: true,
+  });
+
   return (
-    <Container style={{ border: '1px solid black' }}>
+    <Container>
       <Row>
         <Col>
-          <Card style={{ width: '18rem', border: '5px solid #8b5b6e' }}>
-            <Card.Body>
-              <Card.Title
-                style={{ fontSize: '24px', fontFamily: 'Faustina, serif' }}
-              >
-                Laptop
-              </Card.Title>
-              <Card.Text
-                style={{ fontSize: '18px', fontFamily: 'Merriwether, serif' }}
-              >
-                Lenovo Ideapad 3 15.6" Touch Screen Laptop
-              </Card.Text>
-              <PlumButton
-                className="float-right"
-                onClick={(e) => {
-                  e.preventDefault();
-                  window.open(
-                    'https://www.bestbuy.com/site/lenovo-ideapad-3-15-6-touch-screen-laptop-intel-core-i3-8gb-memory-256gb-ssd-almond/6461981.p?skuId=6461981',
-                    '_blank'
-                  );
-                }}
-                style={{ float: 'right' }}
-                variant="primary"
-              >
-                Claim
-              </PlumButton>
-            </Card.Body>
-          </Card>
+          {allValues.laptop ? (
+            <Card style={{ width: '18rem', border: '5px solid #8b5b6e' }}>
+              <Card.Body>
+                <Card.Title
+                  style={{ fontSize: '24px', fontFamily: 'Faustina, serif' }}
+                >
+                  Laptop
+                </Card.Title>
+                <Card.Text
+                  style={{ fontSize: '18px', fontFamily: 'Merriwether, serif' }}
+                >
+                  Lenovo Ideapad 3 15.6&quot; Touch Screen Laptop
+                </Card.Text>
+                <PlumButton
+                  name="laptop"
+                  className="float-right"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    console.log(e.target.name);
+                    setAllValues({
+                      ...allValues,
+                      [e.target.name]: false,
+                    });
+
+                    window.open(
+                      'https://www.bestbuy.com/site/lenovo-ideapad-3-15-6-touch-screen-laptop-intel-core-i3-8gb-memory-256gb-ssd-almond/6461981.p?skuId=6461981',
+                      '_blank'
+                    );
+                  }}
+                  style={{ float: 'right' }}
+                  variant="primary"
+                >
+                  Claim
+                </PlumButton>
+              </Card.Body>
+            </Card>
+          ) : null}
         </Col>
         <Col>
-          <Card style={{ width: '18rem', border: '5px solid #8b5b6e' }}>
-            <Card.Body>
-              <Card.Title
-                style={{ fontSize: '24px', fontFamily: 'Faustina, serif' }}
-              >
-                Blender
-              </Card.Title>
-              <Card.Text
-                style={{ fontSize: '18px', fontFamily: 'Merriwether, serif' }}
-              >
-                Ninja - Professional Plus Kitchen System with Auto-iQ
-              </Card.Text>
-              <PlumButton
-                className="float-right"
-                onClick={(e) => {
-                  e.preventDefault();
-                  window.open(
-                    'https://www.bestbuy.com/site/ninja-professional-plus-kitchen-system-with-auto-iq-black-stainless-steel/6395884.p?skuId=6395884',
-                    '_blank'
-                  );
-                }}
-                style={{ float: 'right' }}
-                variant="primary"
-              >
-                Claim
-              </PlumButton>
-            </Card.Body>
-          </Card>
+          {allValues.blender ? (
+            <Card style={{ width: '18rem', border: '5px solid #8b5b6e' }}>
+              <Card.Body>
+                <Card.Title
+                  style={{ fontSize: '24px', fontFamily: 'Faustina, serif' }}
+                >
+                  Blender
+                </Card.Title>
+                <Card.Text
+                  style={{ fontSize: '18px', fontFamily: 'Merriwether, serif' }}
+                >
+                  Ninja - Professional Plus Kitchen System with Auto-iQ
+                </Card.Text>
+                <PlumButton
+                  name="blender"
+                  className="float-right"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setAllValues({
+                      ...allValues,
+                      [e.target.name]: false,
+                    });
+                    window.open(
+                      'https://www.bestbuy.com/site/ninja-professional-plus-kitchen-system-with-auto-iq-black-stainless-steel/6395884.p?skuId=6395884',
+                      '_blank'
+                    );
+                  }}
+                  style={{ float: 'right' }}
+                  variant="primary"
+                >
+                  Claim
+                </PlumButton>
+              </Card.Body>
+            </Card>
+          ) : null}
         </Col>
         <Col>
-          <Card style={{ width: '18rem', border: '5px solid #8b5b6e' }}>
-            <Card.Body>
-              <Card.Title
-                style={{ fontSize: '24px', fontFamily: 'Faustina, serif' }}
-              >
-                Drip
-              </Card.Title>
-              <Card.Text
-                style={{ fontSize: '18px', fontFamily: 'Merriwether, serif' }}
-              >
-                コラボウェディングドレスが完成いたしました。
-              </Card.Text>
-              <PlumButton
-                className="float-right"
-                onClick={(e) => {
-                  e.preventDefault();
-                  window.open(
-                    'http://www.mariarosa.co.jp/sailormoon/#SLM0006%20Blue-1',
-                    '_blank'
-                  );
-                }}
-                style={{ float: 'right' }}
-                variant="primary"
-              >
-                Claim
-              </PlumButton>
-            </Card.Body>
-          </Card>
+          {allValues.drip ? (
+            <Card style={{ width: '18rem', border: '5px solid #8b5b6e' }}>
+              <Card.Body>
+                <Card.Title
+                  style={{ fontSize: '24px', fontFamily: 'Faustina, serif' }}
+                >
+                  Drip
+                </Card.Title>
+                <Card.Text
+                  style={{ fontSize: '18px', fontFamily: 'Merriwether, serif' }}
+                >
+                  コラボウェディングドレスが完成いたしました。
+                </Card.Text>
+                <PlumButton
+                  name="drip"
+                  className="float-right"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setAllValues({
+                      ...allValues,
+                      [e.target.name]: false,
+                    });
+                    window.open(
+                      'http://www.mariarosa.co.jp/sailormoon/#SLM0006%20Blue-1',
+                      '_blank'
+                    );
+                  }}
+                  style={{ float: 'right' }}
+                  variant="primary"
+                >
+                  Claim
+                </PlumButton>
+              </Card.Body>
+            </Card>
+          ) : null}
         </Col>
       </Row>
     </Container>
