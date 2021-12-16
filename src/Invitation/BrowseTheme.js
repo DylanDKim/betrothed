462e8d79c7d35428a2ccead01ba5fd5a4a8e5828
junma <junma0704@gmail.com>
2021-12-16 15:21:57 -0800

    update style for browse themes

--- src/Invitation/BrowseTheme.js
+++ src/Invitation/BrowseTheme.js
@@ -9,31 +9,34 @@ export default function BrowseTheme() {
   return (
     <Container className="mt-5">
       <Row>
-        <Col xs lg="9">
-          <ProgressBar now={50} />
+        <Col xs lg="9" className="text-center">
+          <div className="d-flex justify-content-center mb-2">
+            <ProgressBar now={50} style={{ width: '50%' }} />
+          </div>
           <BAlexBrushH1>Wedding Website</BAlexBrushH1>
           <h2>Welcome to your Wedding Website</h2>
-          <h5>
-            <i className="fas fa-gift" /> Customize your invitation with themes
-            and colors
-          </h5>
-          <h5>
-            <i className="far fa-star" />
-            Add a cover photo and fill your gallery
-          </h5>
-          <h5>
-            <i className="fas fa-mouse-pointer" />
-            Write a personal message for your guests
-          </h5>
+          <div className="mt-5">
+            <h5>
+              <i className="fas fa-gift" /> Customize your invitation with
+              themes and colors
+            </h5>
+            <h5>
+              <i className="far fa-star" />
+              Add a cover photo and fill your gallery
+            </h5>
+            <h5>
+              <i className="fas fa-mouse-pointer" />
+              Write a personal message for your guests
+            </h5>
+          </div>
           <Link to="/choose-content">
-            <BlueButton>browse themes</BlueButton>
+            <BlueButton className="my-5">browse themes</BlueButton>
           </Link>
         </Col>
         <Col>
           <Image
-            src="http://tineye.com/images/widgets/mona.jpg"
+            src="https://images.unsplash.com/photo-1602705399214-d2fed251f3b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
             fluid
-            style={{ height: '100%' }}
           />
         </Col>
       </Row>
