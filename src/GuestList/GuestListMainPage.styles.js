9c768940c03d5b1b828d565715ea8480d985002f
oliviaschwerdt <okschwerdtfeger@gmail.com>
2021-12-17 01:35:30 -0800

    Add more styles to guest list main page

---
+++ src/GuestList/GuestListMainPage.styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const GuestBMerr18 = styled.tr`
+  font-family: 'Faustina';
+  font-size: 18px;
+  margin: 0px;
+`;
+
+const GuestBMerr24 = styled.div`
+  font-family: 'Faustina';
+  font-size: 24px;
+  margin: 0px;
+  text-align: center;
+`;
+
+export { GuestBMerr18, GuestBMerr24 };
