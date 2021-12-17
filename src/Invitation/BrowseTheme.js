fe56ec9072c6b11755b81b356e64b462e8f4fc05
junma <junma0704@gmail.com>
2021-12-16 19:54:03 -0800

    WIP Invitation styling

--- src/Invitation/BrowseTheme.js
+++ src/Invitation/BrowseTheme.js
@@ -3,35 +3,41 @@ import { Link } from 'react-router-dom';
 import { ProgressBar, Container, Row, Col, Image } from 'react-bootstrap';
 import BlueButton from '../Common/styled/buttonstyles/BlueButton';
 import { BAlexBrushH1 } from '../Common/styled/textstyles/AlexBrushH1';
+import { BFaustinaH3 } from '../Common/styled/textstyles/FaustinaH3';
+import { BMerr18 } from '../Common/styled/textstyles/Merr18';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function BrowseTheme() {
   return (
     <Container className="mt-5">
       <Row>
-        <Col xs lg="9" className="text-center">
+        <Col xs lg="9">
           <div className="d-flex justify-content-center mb-2">
-            <ProgressBar now={50} style={{ width: '50%' }} />
+            <ProgressBar now={50} style={{ width: '50%', color: 'yellow' }} />
           </div>
-          <BAlexBrushH1>Wedding Website</BAlexBrushH1>
-          <h2>Welcome to your Wedding Website</h2>
-          <div className="mt-5">
-            <h5>
-              <i className="fas fa-gift" /> Customize your invitation with
-              themes and colors
-            </h5>
-            <h5>
-              <i className="far fa-star" />
-              Add a cover photo and fill your gallery
-            </h5>
-            <h5>
-              <i className="fas fa-mouse-pointer" />
-              Write a personal message for your guests
-            </h5>
+          <div style={{ marginLeft: '25%' }}>
+            <BAlexBrushH1 className="mt-3">Wedding Website</BAlexBrushH1>
+            <BFaustinaH3 className="mb-5">
+              Welcome to your Wedding Website
+            </BFaustinaH3>
+            <div className="mt-5">
+              <BMerr18>
+                <i className="fas fa-gift" /> Customize your invitation with
+                themes and colors
+              </BMerr18>
+              <BMerr18>
+                <i className="far fa-star" />
+                Add a cover photo and fill your gallery
+              </BMerr18>
+              <BMerr18>
+                <i className="fas fa-mouse-pointer" />
+                Write a personal message for your guests
+              </BMerr18>
+            </div>
+            <Link to="/event/1/choose-content">
+              <BlueButton className="my-5">browse themes</BlueButton>
+            </Link>
           </div>
-          <Link to="/event/1/choose-content">
-            <BlueButton className="my-5">browse themes</BlueButton>
-          </Link>
         </Col>
         <Col>
           <Image
