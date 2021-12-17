0b415230601f0c0ed537cf92325743d7b9b1fe1d
oliviaschwerdt <okschwerdtfeger@gmail.com>
2021-12-17 14:11:55 -0800

    Add post route

--- server/app.js
+++ server/app.js
@@ -22,6 +22,26 @@ app.get('/guestlist', (req, res) => {
     .catch((error) => error);
 });
 
+app.post('/guestlist', (req, res) => {
+  const { eventId } = req.query;
+  const { firstName, lastName, email, group = 'Individual' } = req.body;
+
+  axios
+    .post(
+      `https://betrothed-server.herokuapp.com/api/events/61b79b9e0ac02dbe3e12fd1b/guests`,
+      {
+        firstName,
+        lastName,
+        email,
+        group,
+      }
+    )
+    .then((response) => {
+      res.status(201).send(response.data);
+    })
+    .catch((error) => console.log(error));
+});
+
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'dist', 'index.html'), (err) => {
     if (err) {
